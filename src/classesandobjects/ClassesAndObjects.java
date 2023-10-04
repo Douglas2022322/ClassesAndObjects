839204f8d591bb038b2cd305917c48d1394b53c3
Douglas Vinicius Dierings <146199335+Douglas2022322@users.noreply.github.com>
2023-10-04 12:46:08 +0100

    Triangle sides in Array
    Made a triangle class and 6 triangle objects.

--- src/classesandobjects/ClassesAndObjects.java
+++ src/classesandobjects/ClassesAndObjects.java
@@ -15,6 +15,33 @@ public class ClassesAndObjects {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Triangle tril1 = new Triangle();
+        Triangle tril2 = new Triangle();
+        Triangle tril3 = new Triangle();
+        Triangle tril4 = new Triangle();
+        Triangle tril5 = new Triangle();
+        Triangle tril6 = new Triangle();
+
+        int[] sideLengths = new int[18];
+        sideLengths[0] = 5;
+        sideLengths[1] = 8;
+        sideLengths[2] = 9;
+        
+        
+        sideLengths[12] = 4;
+        sideLengths[13] = 7;
+        sideLengths[14] = 2;
+        
+        
+        sideLengths[1] = 3;
+        sideLengths[4] = 3;
+        sideLengths[7] = 3;
+        sideLengths[10] = 3;
+        sideLengths[13] = 3;
+        sideLengths[16] = 3;
+        
+        
+        
     }
-    
+
 }
